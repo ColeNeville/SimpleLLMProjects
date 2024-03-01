2d60b117fda71ebf87df3e6b4d6f5978696cf34b
ColeNeville <colewneville2@gmail.com>
2024-03-01 16:32:36 -0700

    Adding flake.nix file

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs @ {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+
+        config.allowUnfree = true;
+      };
+    in {
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python311
+            python311Packages.pip
+            pipenv
+            docker-compose
+          ];
+        };
+      };
+    }
+  );
+}
